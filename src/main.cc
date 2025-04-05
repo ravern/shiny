74bd541005da5f1684439c9743695771833e21e7
Dexter Leng <34204380+dexterleng@users.noreply.github.com>
2025-04-05 16:06:24 +0800

    Add REPL

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,6 @@
+#include <argparse/argparse.hpp>
+#include <fstream>
+
 #include "frontend/ast_pretty_printer.h"
 #include "frontend/compiler.h"
 #include "frontend/parser.h"
@@ -7,40 +10,72 @@
 #include "runtime/value.h"
 #include "vm/vm.h"
 
-int main() {
-  StringInterner interner;
+StringInterner interner;
 
-  std::string source = R"(
-  func foo(x: Int, y: Int) -> Int {
-    var z = x + y
-    return z + x
+void interpret(const std::string& source) {
+  try {
+    Scanner scanner(source);
+    Parser parser(scanner, interner);
+    auto ast = parser.parse();
+    if (parser.hadError()) {
+      return;
+    }
+
+    TypeInference inference(interner);
+    inference.perform(*ast);
+
+    ASTPrettyPrinter printer(interner);
+    printer.print(*ast);
+
+    Compiler compiler(nullptr, Compiler::FunctionKind::TopLevel, interner, *ast);
+    auto rootFunction = ObjectPtr<FunctionObject>(compiler.compile());
+
+    VM vm;
+    vm.evaluate(rootFunction);
+  } catch (const Error& e) {
+    std::cout << "Error: " << e.what() << std::endl;
   }
+}
 
-  func bar(x: Int, y: Int) -> Int {
-    return x + y
+void runRepl() {
+  std::cout << "Shiny REPL. Type 'exit' to quit.\n";
+  std::string input;
+  while (true) {
+    std::cout << "> ";
+    std::getline(std::cin, input);
+    if (input == "exit") break;
+    interpret(input);
   }
+}
 
-  var z = foo(3, bar(4, 5))
-  )";
+void runFile(const std::string& filename) {
+  std::ifstream file(filename);
+  if (!file) {
+    std::cerr << "Could not open file: " << filename << std::endl;
+    exit(1);
+  }
+  std::string input((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+  interpret(input);
+}
 
-  Scanner scanner(source);
-  Parser parser(scanner, interner);
-  auto ast = parser.parse();
-  if (parser.hadError()) {
+int main(int argc, char** argv) {
+  argparse::ArgumentParser program("shiny");
+  program.add_argument("file")
+    .help("shiny file")
+    .nargs(argparse::nargs_pattern::optional);
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
     return 1;
   }
 
-  TypeInference inference(interner);
-  inference.perform(*ast);
-
-  ASTPrettyPrinter printer(interner);
-  printer.print(*ast);
-
-  Compiler compiler(nullptr, Compiler::FunctionKind::TopLevel, interner, *ast);
-  auto rootFunction = ObjectPtr<FunctionObject>(compiler.compile());
-
-  VM vm;
-  vm.evaluate(rootFunction);
-
+  if (program.present("file")) {
+    runFile(program.get<std::string>("file"));
+  } else {
+    runRepl();
+  }
   return 0;
 }
