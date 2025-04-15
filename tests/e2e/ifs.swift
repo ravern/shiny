7d8734cda8e2269f59d27648608a08df1242ace8
Ravern Koh <ravernkoh@gmail.com>
2025-04-15 17:24:05 +0800

    add e2e test for lexical scoping

---
+++ tests/e2e/ifs.swift
@@ -0,0 +1,22 @@
+func foo() -> Int {
+    var x = 6
+    if x == 6 {
+        var a = 313
+        var b = 343
+        var c = 33
+    }
+    var y = 8
+    if y == 8 {
+        var a = 3
+        var b = 6
+        return 4
+        var c = 34
+    }
+    var z = 21
+    if z == 3 {
+        return 5
+    }
+    return 6
+}
+
+foo()
