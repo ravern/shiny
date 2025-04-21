3163775c805226bbbf9c5590904357094b44bc56
Ravern Koh <ravernkoh@gmail.com>
2025-04-21 23:27:24 +0800

    method call e2e

---
+++ tests/e2e/method_calls.swift
@@ -0,0 +1,17 @@
+class Foo {
+    var x = 0
+    var y = 0
+
+    func bar(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+
+    func baz() -> Int {
+        self.x + self.y
+    }
+}
+
+var foo = Foo()
+foo.bar(1, 2)
+foo.baz()
